c8f5b675fd3e1f1a2527b3143ff32d51fd0dc073
namsoo5 <are34@naver.com>
2021-05-31 22:05:52 +0900

    CREATE: 프로젝트 생성
    - 타겟 10.0

---
+++ prac_flo/prac_flo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  prac_flo
+//
+//  Created by 남수김 on 2021/05/31.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ prac_flo/prac_flo/MusicPlayerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prac_flo
+//
+//  Created by 남수김 on 2021/05/31.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
